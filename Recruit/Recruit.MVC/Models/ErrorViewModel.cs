4f6c4b666b96a712d365d706e0e29289601a11e7
Luis Fernando <parido@gmail.com>
2018-04-16 09:30:11 -0600

    Fix the solution
    fix the solution

---
+++ Recruit/Recruit.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recruit.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
