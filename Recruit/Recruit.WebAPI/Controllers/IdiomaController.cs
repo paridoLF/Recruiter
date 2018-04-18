28b1b55b1ba9f2450f3017ebfb235f4bc8058744
earceh <38363604+earceh@users.noreply.github.com>
2018-04-18 16:39:48 -0600

    Cambios en DBcontroller
    Cambios en DBcontroller

--- Recruit/Recruit.WebAPI/Controllers/IdiomaController.cs
+++ Recruit/Recruit.WebAPI/Controllers/IdiomaController.cs
@@ -12,7 +12,14 @@ namespace Recruit.WebAPI.Controllers
     [Route("api/[controller]")]
     public class IdiomaController : Controller
     {
-        private readonly RecruitContext _context;
+        private readonly Recruit_DBContext _context;
+
+
+        public IdiomaController(Recruit_DBContext context)
+        {
+            _context = context;
+
+        }
         // GET: api/Idioma
         [HttpGet]
         public IEnumerable<TRecIdioma> Get()
