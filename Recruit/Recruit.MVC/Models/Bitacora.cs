da8bd1f020fab785f9262214ec2ee0131dcef7f0
erapso <38363694+erapso@users.noreply.github.com>
2018-04-23 14:27:58 -0600

    Bitacora
    Bitacora

---
+++ Recruit/Recruit.MVC/Models/Bitacora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recruit.WebAPI.Models 
+{
+    public partial class Bitacora
+    {
+        public int Pkbitacora { get; set; }
+        public string Accionbitacora { get; set; }
+        public string Tablabitacora { get; set; }
+        public DateTime Fechabitacora { get; set; }
+        public string Registrobitacora { get; set; }
+        public string Paginabitacora { get; set; }
+    }
+}
