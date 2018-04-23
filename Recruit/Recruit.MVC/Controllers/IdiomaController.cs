9224193cf390919e15fa04d40e5ef9c638fed880
earceh <38363604+earceh@users.noreply.github.com>
2018-04-23 16:19:50 -0600

    Actualización de controllers Idioma y Estado
    Actualización de controllers Idioma y Estado

--- Recruit/Recruit.MVC/Controllers/IdiomaController.cs
+++ Recruit/Recruit.MVC/Controllers/IdiomaController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Recruit.MVC.Models;
 
 namespace Recruit.MVC.Controllers
 {
     public class IdiomaController : Controller
     {
-        public IActionResult Index()
+        string apiURL = "http://localhost:53908/";
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Idioma> idiomaList = new List<Idioma>();
+
+            using (var idioma = new HttpClient())
+            {
+                idioma.BaseAddress = new Uri(apiURL);
+                idioma.DefaultRequestHeaders.Accept.Clear();
+                idioma.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage res = await idioma.GetAsync("api/Idioma");
+
+                if(res.IsSuccessStatusCode)
+                {
+                    var idiomaResult = res.Content.ReadAsStringAsync().Result;
+
+                    idiomaList = JsonConvert.DeserializeObject<List<Idioma>>(idiomaResult);
+                }
+            }
+
+               return View();
         }
 
         public IActionResult Create()
