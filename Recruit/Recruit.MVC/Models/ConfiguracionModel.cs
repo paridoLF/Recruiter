5652576a5ec5c2e1683145bec961782e7379fe4b
ruvargo <ruvargo@users.noreply.github.com>
2018-04-23 14:32:56 -0600

    Modelo de Configuracion

---
+++ Recruit/Recruit.MVC/Models/ConfiguracionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruit.MVC.Models
+{
+    public class ConfiguracionModel
+    {
+        public string CORREOCONFIGURACION { get; set; }
+        public string PATHCONFIGURACION { get; set; }
+        public int PKCONFIGURACION { get; set; }
+    }
+}
