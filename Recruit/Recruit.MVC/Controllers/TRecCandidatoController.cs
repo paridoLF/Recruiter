2890ac9fac4d272747dcebf06cfb763ffb3ae0a8
KrisEl0982 <38363486+KrisEl0982@users.noreply.github.com>
2018-04-23 16:13:17 -0600

    Candidato controller Index
    Candidato controller Index

--- Recruit/Recruit.MVC/Controllers/TRecCandidatoController.cs
+++ Recruit/Recruit.MVC/Controllers/TRecCandidatoController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace Recruit.MVC.Controllers
 {
     public class TRecCandidatoController : Controller
     {
+        string apUrl = "http://localhost:53917/";
         public IActionResult Index()
         {
-            return View();
+            //List<TRecCandidatoController>
+            //using (var candidato = new HttpClient())
+            //{
+            //    candidato.BaseAddress = new Uri("http://localhost:53917/");
+            //    candidato.DefaultRequestHeaders.Accept.Clear();
+            //    candidato.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/Json"));
+
+            //    HttpResponseMessage response = await candidato.GetAsync("api/TRecCandidato");
+
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        var 
+            //    }
+
+            //}
+               
+
+                return View();
         }
 
         public IActionResult Create()
